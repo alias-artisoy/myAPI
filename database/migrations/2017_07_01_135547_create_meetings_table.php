cba747188fee0ee4c7e789f680440b433ea787a0
artfernandez <artisoyfernandez@gmail.com>
2017-07-01 22:15:57 +0800

    Make Meeting model and migrate table.

--- database/migrations/2017_07_01_135547_create_meetings_table.php
+++ database/migrations/2017_07_01_135547_create_meetings_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateMeetingsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('meetings', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email');
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
+            $table->dateTime('time');
+            $table->string('title');
+            $table->text('description');
         });
     }
 
@@ -30,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('meetings');
     }
 }
--- database/migrations/2017_07_01_135659_create_meeting_user_table.php
+++ database/migrations/2017_07_01_135659_create_meeting_user_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateMeetingUserTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('meeting_user', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email');
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
+            $table->integer('user_id');
+            $table->integer('meeting_id');
         });
     }
 
@@ -30,6 +28,7 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('meeting_user');
+    
     }
 }
