eb803cc3a5351bd68da0a77db579b381a4dd3999
artfernandez <artisoyfernandez@gmail.com>
2017-07-01 15:45:53 +0800

    Modify AuthController to access input data.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,10 +7,15 @@ use Illuminate\Http\Request;
 class AuthController extends Controller
 {
     public function store(Request $request){
+        $name = $request->input('name');
+        $email = $request->input('email');
+        $password = $request->input('password');
         return "It works.";
     }
 
     public function signin(Request $request){
+        $email = $request->input('email');
+        $password = $request->input('password');
         return "It works.";
     }
     
