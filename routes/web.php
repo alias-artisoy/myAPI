30dc52756f104f6836f9e18b7fd29bba602cc41c
artfernandez <artisoyfernandez@gmail.com>
2017-07-01 15:03:20 +0800

    Initialized API.

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::group(['prefix' => 'api/v1'], function(){
+
+    Route::resource('meeting', 'MeetingController',[
+        'except' => ['edit','create']
+    ]);
+
+    Route::resource('meeting/registration', 'RegistrationController',[
+        'except' => ['store','destroy']
+    ]);
+
+    Route::post('user', [
+        'uses' => 'AuthController@store'
+    ]);
+
+    Route::post('user/signin', [
+        'uses' => 'AuthController@store'
+    ]);
+
+});
